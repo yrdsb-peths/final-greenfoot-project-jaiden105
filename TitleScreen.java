cb7d803fb1b3fbb6e8b7cdc392855fc8943eb8bb
ashmcdgang <340867134@gapps.yrdsb.ca>
2023-05-29 22:34:26 -0400

    Title Screen Changes
    Started the title screen by adding the title and background, also added in idle stances for the two players in the game.

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-
+    Label titleLabel = new Label("Toy Story", 120);
+    Label titleLabel2 = new Label("Battlegrounds", 110);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -16,6 +17,20 @@ public class TitleScreen extends World
     public TitleScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1280, 720, 1); 
+        addObject(titleLabel, getWidth()/2, (getHeight()/2)-300);
+        addObject(titleLabel2, getWidth()/2, (getHeight()/2)-200);
+        //prepare();
+    }
+    
+    /**
+     * The main world act loop
+     */
+    public void act() {
+        //Starts game if user presses space
+        if(Greenfoot.isKeyDown("space")) {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
     }
 }
